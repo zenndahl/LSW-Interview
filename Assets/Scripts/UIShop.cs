c0cafa9d898a65a7b4c1a304d03301d0141e8006
zenndahl <50121981+zenndahl@users.noreply.github.com>
2022-08-28 09:25:11 -0300

    Spritesheets sliced, working on the shop

--- Assets/Scripts/UIShop.cs
+++ Assets/Scripts/UIShop.cs
@@ -25,29 +25,48 @@ public class UIShop : MonoBehaviour
         if (isOutfit)
         {
             //CreateItemButton(Outfit.GetSprite(Outfit.OutfitType.YellowShirt), "Yellow Shirt", Outfit.GetCost(Outfit.OutfitType.YellowShirt), 0);
-            CreateItemButton(Outfit.GetSprite(Outfit.OutfitType.OrangeShirt), "Orange Shirt", Outfit.GetCost(Outfit.OutfitType.OrangeShirt), 1);
-            CreateItemButton(Outfit.GetSprite(Outfit.OutfitType.RedShirt), "Red Shirt", Outfit.GetCost(Outfit.OutfitType.RedShirt), 2);
+            CreateItemButton(Accessory.AccessoryType.NoAccessory, Outfit.OutfitType.OrangeShirt, Outfit.GetSprite(Outfit.OutfitType.OrangeShirt), "Orange Shirt", Outfit.GetCost(Outfit.OutfitType.OrangeShirt), 1);
+            CreateItemButton(Accessory.AccessoryType.NoAccessory, Outfit.OutfitType.RedShirt, Outfit.GetSprite(Outfit.OutfitType.RedShirt), "Red Shirt", Outfit.GetCost(Outfit.OutfitType.RedShirt), 2);
         }
         else
         {
             //CreateItemButton(Accessory.GetSprite(Accessory.AccessoryType.YellowFlowerCirclet), "Yellow Flower Circlet", Accessory.GetCost(Accessory.AccessoryType.YellowFlowerCirclet), 1);
-            CreateItemButton(Accessory.GetSprite(Accessory.AccessoryType.OrangeFlowerCirclet), "Orange Flower Circlet", Accessory.GetCost(Accessory.AccessoryType.OrangeFlowerCirclet), 2);
-            CreateItemButton(Accessory.GetSprite(Accessory.AccessoryType.RedFlowerCirclet), "Red Flower Circlet", Accessory.GetCost(Accessory.AccessoryType.RedFlowerCirclet), 3);
-            CreateItemButton(Accessory.GetSprite(Accessory.AccessoryType.FarmerHat), "Farmer Hat", Accessory.GetCost(Accessory.AccessoryType.FarmerHat), 4);
+            CreateItemButton(Accessory.AccessoryType.OrangeFlowerCirclet, Outfit.OutfitType.WhiteShirt, Accessory.GetSprite(Accessory.AccessoryType.OrangeFlowerCirclet), "Orange Flower Circlet", Accessory.GetCost(Accessory.AccessoryType.OrangeFlowerCirclet), 2);
+            CreateItemButton(Accessory.AccessoryType.RedFlowerCirclet, Outfit.OutfitType.WhiteShirt, Accessory.GetSprite(Accessory.AccessoryType.RedFlowerCirclet), "Red Flower Circlet", Accessory.GetCost(Accessory.AccessoryType.RedFlowerCirclet), 3);
+            CreateItemButton(Accessory.AccessoryType.FarmerHat, Outfit.OutfitType.WhiteShirt, Accessory.GetSprite(Accessory.AccessoryType.FarmerHat), "Farmer Hat", Accessory.GetCost(Accessory.AccessoryType.FarmerHat), 4);
         }
     }
 
-    private void CreateItemButton(Sprite itemSprite, string itemName, int itemCost, int positionIndex)
+    private void CreateItemButton(Accessory.AccessoryType accessoryType, Outfit.OutfitType outfitType, Sprite itemSprite, string itemName, int itemCost, int positionIndex)
     {
         Transform shopItemTransform = Instantiate(shopItemTemplate, transform);
-        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+        //RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
 
-        float shopItemHeight = 30f;
-        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+        //float shopItemHeight = 30f;
+        //shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
 
         shopItemTransform.Find("ItemName").GetComponent<TextMeshProUGUI>().SetText(itemName);
         shopItemTransform.Find("ItemCost").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
 
         shopItemTransform.Find("ItemImage").GetComponent<Image>().sprite = itemSprite;
+
+        if (isOutfit)
+        {
+            shopItemTransform.GetComponent<Button>().onClick.AddListener(delegate { TryBuyOutfit(outfitType); });
+        }
+        else
+        {
+            shopItemTransform.GetComponent<Button>().onClick.AddListener(delegate { TryBuyAccessory(accessoryType); });
+        }
+    }
+
+    public void TryBuyAccessory(Accessory.AccessoryType accessoryType)
+    {
+
+    }
+    
+    public void TryBuyOutfit(Outfit.OutfitType outfitType)
+    {
+
     }
 }
